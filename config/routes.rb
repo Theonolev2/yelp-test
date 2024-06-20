cc2bc57b14268dde9a1f4be7dd0bbb20671f92f3
Theo Velon <theovelon@hotmail.fr>
2024-06-20 12:13:12 +0200

    yelp-test prep 2

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :restaurants, only: [:index, :new, :create, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:new, :create]
   end
 end
